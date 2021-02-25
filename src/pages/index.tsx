fc6c3040ef2b29af12926987788ac0c74c36f4af
lucasdgimenez <lucas_d360@hotmail.com>
2021-02-24 21:48:37 -0300

    componente de cronometro, perfil, desafios completados e organizei o css para melhor gerenciamento

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,24 @@
+import { CompletedChallenges } from '../components/COmpletedChallenges'
+import { Countdown } from '../components/Countdown'
 import { ExperienceBar } from '../components/ExpirienceBar'
 import { Profile } from '../components/Profile'
 import Text from '../components/Text'
+import Head from "next/head"
 import styles from "../styles/pages/Home.module.css"
 
 export default function Home() {
   return (
     <div className={styles.container}>
-    
+      <Head>
+        <title>Inicio | moveit</title>
+      </Head>
       <ExperienceBar/>
 
       <section>
         <div>
           <Profile/>
+          <CompletedChallenges/>
+          <Countdown/>
         </div>
         <div></div>
       </section>
